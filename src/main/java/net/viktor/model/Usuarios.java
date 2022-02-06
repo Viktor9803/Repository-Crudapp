99e9baa8961b7aab6fb7ce504bcbde3a105c6433
Victor Hernández <Viktorjhm@gmail.com>
2022-02-06 19:50:00 -0400

    Se agrego a la clase de modelo las anotaciones necesarias @Entity, @Table() a la clase, E @Id, @GeneratedValue(Strategy=GenerationType.IDENTITY) a la clave primaria

--- src/main/java/net/viktor/model/Usuarios.java
+++ src/main/java/net/viktor/model/Usuarios.java
@@ -1,7 +1,17 @@
 package net.viktor.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
 public class Usuarios {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String apellido;
