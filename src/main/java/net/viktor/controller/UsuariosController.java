05202eb4ef2c407d2750abe1bd9ac73029307e26
Victor Hernández <Viktorjhm@gmail.com>
2022-02-06 21:39:49 -0400

    Se agregaron archivos: Bootstrap, Se creo la vista index.html y se agrego la funcion de registro al controlador v1.0.0

--- src/main/java/net/viktor/controller/UsuariosController.java
+++ src/main/java/net/viktor/controller/UsuariosController.java
@@ -3,8 +3,10 @@ package net.viktor.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.viktor.model.Usuarios;
 import net.viktor.repository.usuariosRepository;
 
 @Controller
@@ -14,9 +16,17 @@ public class UsuariosController {
 	@Autowired
 	private usuariosRepository repo;
 	
-	public String Registro(Model model) {
-		
-		return null;		
+	@GetMapping("/create")
+	public String Registro() {
+		Usuarios user = new Usuarios();
+		user.setNombre("name");
+		user.setApellido("lname");
+		user.setNusuario("user1");
+		user.setPasswork("1234");
+		user.setGenero(1);
+		repo.save(user);
+		System.out.println(user);
+		return "crudusuarios/index";		
 	}
 
 }
