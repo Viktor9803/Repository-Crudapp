ebb9648eab008b1093d64928e63ed01098109e35
Victor Hernández <Viktorjhm@gmail.com>
2022-02-07 17:36:35 -0400

    Se agrego correctamente la funcionalidad de update al controlador, se añadio la vista correspondiente v1.2.0

--- src/main/java/net/viktor/controller/UsuariosController.java
+++ src/main/java/net/viktor/controller/UsuariosController.java
@@ -1,5 +1,7 @@
 package net.viktor.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,24 +18,57 @@ public class UsuariosController {
 	
 	@Autowired
 	private usuariosRepository repo;
+	Usuarios user = new Usuarios();
 	
-	@GetMapping("/index")
+	@GetMapping("/create")
 	public String mostrarRegistro() {
 		
 		return "crudusuarios/index";		
 	}
 	
-	@PostMapping("/create")
+	@GetMapping("/update")
+	public String mostrarActualizacion() {
+		
+		
+		return "crudusuarios/actualizar";
+	}
+	
+	
+	/*
+	 * Método de actualización de registro en la bd (update)
+	 */
+	@PostMapping("/actualizado")
+	public String Actualizar(@RequestParam("nombre") String nombretmp, @RequestParam("apellido") String apellidotmp, @RequestParam("nusuario") String nusuariotmp, @RequestParam("passwork") String passworktmp, @RequestParam("genero") String generotmp) {
+		Optional<Usuarios> option = repo.findById(10);
+		if (option.isPresent()) {
+			Usuarios usertemp = option.get();
+			usertemp.setNombre(nombretmp);
+			usertemp.setApellido(apellidotmp);
+			usertemp.setNusuario(nusuariotmp);
+			usertemp.setPasswork(passworktmp);
+			usertemp.setGenero(generotmp);
+				repo.save(usertemp);
+			System.out.println(option.get());
+		} else {
+			System.out.println("Usuario no encontrado.");
+		}
+		
+		return "crudusuarios/lista";		
+	}
+	
+	/* *
+	 * Método de registro en la bd (Create).
+	 * */
+	@PostMapping("/registrado")
 	public String Registro(@RequestParam("nombre") String nombretmp, @RequestParam("apellido") String apellidotmp, @RequestParam("nusuario") String nusuariotmp, @RequestParam("passwork") String passworktmp, @RequestParam("genero") String generotmp) {
-		//Debo agregar el <<<<@RequetParam("genero") Integer generotmp>>>> a la clase
-		Usuarios user = new Usuarios();
 		user.setNombre(nombretmp);
 		user.setApellido(apellidotmp);
 		user.setNusuario(nusuariotmp);
 		user.setPasswork(passworktmp);
 		user.setGenero(generotmp);
-		repo.save(user);
+			repo.save(user);
 		System.out.println(user);
+			
 		return "crudusuarios/lista";		
 	}
 
