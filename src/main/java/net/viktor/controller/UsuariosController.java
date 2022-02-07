6f489ecbf2cc9d9e1f705e128e8834cf0ddf5270
Victor Hernández <Viktorjhm@gmail.com>
2022-02-06 22:54:26 -0400

    Se  modifico el controlador para que bajara los datos directo de la vista y luego llevarlos a la bd, se estilizó el index,html, agregado diseños leves y las relacion de los imputs con las variables temporales del controlador, finalmente se creo una nueva vista.

--- src/main/java/net/viktor/controller/UsuariosController.java
+++ src/main/java/net/viktor/controller/UsuariosController.java
@@ -2,9 +2,10 @@ package net.viktor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.viktor.model.Usuarios;
 import net.viktor.repository.usuariosRepository;
@@ -16,17 +17,23 @@ public class UsuariosController {
 	@Autowired
 	private usuariosRepository repo;
 	
-	@GetMapping("/create")
-	public String Registro() {
+	@GetMapping("/index")
+	public String mostrarRegistro() {
+		
+		return "crudusuarios/index";		
+	}
+	
+	@PostMapping("/create")
+	public String Registro(@RequestParam("nombre") String nombretmp, @RequestParam("apellido") String apellidotmp, @RequestParam("nusuario") String nusuariotmp, @RequestParam("passwork") String passworktmp) {
 		Usuarios user = new Usuarios();
-		user.setNombre("name");
-		user.setApellido("lname");
-		user.setNusuario("user1");
-		user.setPasswork("1234");
+		user.setNombre(nombretmp);
+		user.setApellido(apellidotmp);
+		user.setNusuario(nusuariotmp);
+		user.setPasswork(passworktmp);
 		user.setGenero(1);
 		repo.save(user);
 		System.out.println(user);
-		return "crudusuarios/index";		
+		return "crudusuarios/lista";		
 	}
 
 }
