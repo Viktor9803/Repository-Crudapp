4dc331afa4d4f74688d393e9412779a4aed8f9ff
Victor Hernández <Viktorjhm@gmail.com>
2022-02-15 21:34:38 -0400

    Optimización de código, eliminación de vistas redundantes, inplementacion de estilos en vistas. v1.5.0

--- src/main/java/net/viktor/controller/UsuariosController.java
+++ src/main/java/net/viktor/controller/UsuariosController.java
@@ -1,10 +1,6 @@
 package net.viktor.controller;
 
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import net.viktor.model.Usuarios;
-import net.viktor.repository.usuariosRepository;
 import net.viktor.services.IUsuariosServices;
 
 @Controller
@@ -25,11 +18,6 @@ public class UsuariosController {
 	
 	@Autowired
 	private IUsuariosServices usuariosServices;
-	
-	@Autowired
-	private usuariosRepository repo;
-	
-	Usuarios user = new Usuarios();
 	
 	//FINDALL
 	@GetMapping("/")
@@ -65,7 +53,7 @@ public class UsuariosController {
 	public String eliminar(@PathVariable("id") Integer id) {
 		usuariosServices.eliminarPorId(id);
 			
-		return "crudusuarios/eliminar";		
+		return "redirect:/users/";		
 	}
 	
 	//READ
@@ -78,6 +66,15 @@ public class UsuariosController {
 		return "/crudusuarios/detalle";		
 	}
 	
+	//DELETEALL
+	@GetMapping("/deleteall")
+	public String Deleteall() {
+		usuariosServices.eliminarTodo();
+		System.out.println("Se han eliminado todos los registros.");
+		
+		return "redirect:/users/";
+	}
+	
 	//CREATE/GUARDAR
 	@PostMapping("/registrado")
 	public String guardar(@ModelAttribute Usuarios usuarios) {
@@ -86,142 +83,4 @@ public class UsuariosController {
 		return "redirect:/users/";		
 	}	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-/*
- * METODOS TIPO GET	
- */
-	
-	@GetMapping("/findid")
-	public String findid() {
-		
-		return "crudusuarios/buscarid";
-	}
-	
-	/*@GetMapping("/create")
-	public String mostrarRegistro() {
-		
-		return "crudusuarios/registrar";		
-	}*/
-	
-	/*@GetMapping("/update/{id}")
-	public String mostrarActualizacion(@PathVariable("id") int idusuario, Model model) {
-		Optional<Usuarios> usuario = repo.findById(idusuario);
-		if (usuario.isPresent()) {
-			model.addAttribute("usuario", usuario);
-			return "crudusuarios/actualizar";
-		}else {
-			return "redirect:/users/";
-		}
-		//return "crudusuarios/actualizar";
-	}*/
-	
-	/*
-	 * Metodo que devuelve desde la base de datos todo los registros y los muestra en la tabla de la vista.
-	 
-	@GetMapping("/")
-	public String Listado(Model model) {
-		List<Usuarios> listusers = new LinkedList<Usuarios>();		
-			Iterable<Usuarios> usuarios = repo.findAll();
-				for (Usuarios usu : usuarios) {
-					listusers.add(usu);
-				}
-				model.addAttribute("usuario", listusers);
-				System.out.println(listusers);
-		
-		return "/crudusuarios/lista";
-	}*/
-	
-	/*
-	 * Metodo Para eliminar todos los registro de la bd (DELETEALL)
-	 */
-	@GetMapping("/deleteall")
-	public String Deleteall() {
-		repo.deleteAll();
-		System.out.println("Se han eliminado todos los registros.");
-		
-		return "crudusuarios/lista";
-	}
-	
-	/*
-	 * Metodo para eliminar un registro de la bd (DELETE) por el id, en la tabla
-	 */
-	/*@GetMapping("/delete/{id}")
-	public String Eliminar(@PathVariable("id") int idusuario, Model model) {
-		model.addAttribute("idusuario", idusuario);
-		repo.deleteById(idusuario);
-		
-		return "crudusuarios/eliminar";		
-	}*/
-	
-	/*
-	 * Metodo para ver detalles de registros en la tabla (READ)
-	 */
-	/*@GetMapping("/detalle/{id}")
-	public String buscarid(@PathVariable("id") int idusuario, Model model) {
-		model.addAttribute("idusuario", idusuario);
-		//Usuarios ustmp = new Usuarios();
-		Optional<Usuarios> opt = repo.findById(idusuario);
-		if (opt.isPresent()) {
-			model.addAttribute("usuario", opt.get());
-			System.out.println("USUARIO: "+opt.get());
-			return "crudusuarios/detalle";
-		}else {
-			System.out.println("Coloque un id para buscar");
-		}
-		return "";
-	}*/
-	
-/*
- * METODOS TIPO POST
- */
-	
-	/*
-	 * Método de actualización de registro en la bd (UPDATE)
-	 
-	@PostMapping("/actualizado")
-	public String Actualizar(@RequestParam("nombre") String nombretmp, @RequestParam("apellido") String apellidotmp, @RequestParam("nusuario") String nusuariotmp, @RequestParam("passwork") String passworktmp, @RequestParam("genero") String generotmp, @PathVariable("id") int idusuario, Model model) {
-		model.addAttribute("idusuario", idusuario);
-		Optional<Usuarios> option = repo.findById(idusuario);
-		if (option.isPresent()) {
-			Usuarios usertemp = option.get();
-			usertemp.setNombre(nombretmp);
-			usertemp.setApellido(apellidotmp);
-			usertemp.setNusuario(nusuariotmp);
-			usertemp.setPasswork(passworktmp);
-			usertemp.setGenero(generotmp);
-				repo.save(usertemp);
-			System.out.println(option.get());
-			return "redirect:/users/";
-		} else {
-			System.out.println("Usuario no encontrado.");
-			return "crudusuarios/actualizar";
-		}		
-	}*/
-	
-	/* *
-	 * Método de registro en la bd (CREATE).
-	 * 
-	@PostMapping("/registrado")
-	public String Registro(@RequestParam("nombre") String nombretmp, @RequestParam("apellido") String apellidotmp, @RequestParam("nusuario") String nusuariotmp, @RequestParam("passwork") String passworktmp, @RequestParam("genero") String generotmp) {
-		Usuarios usercreate = new Usuarios();
-		usercreate.setNombre(nombretmp);
-		usercreate.setApellido(apellidotmp);
-		usercreate.setNusuario(nusuariotmp);
-		usercreate.setPasswork(passworktmp);
-		usercreate.setGenero(generotmp);
-			repo.save(usercreate);
-		System.out.println(usercreate);
-			
-		return "redirect:/users/";		
-	}*/
-
 }
---
+++ src/main/java/net/viktor/controller/antiguoController.java
@@ -0,0 +1,136 @@
+package net.viktor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class antiguoController {
+	
+	/*
+	 * METODOS TIPO GET	
+	 */
+		
+		/*@GetMapping("/findid")
+		public String findid() {
+			
+			return "crudusuarios/buscarid";
+		}*/
+		
+		/*@GetMapping("/create")
+		public String mostrarRegistro() {
+			
+			return "crudusuarios/registrar";		
+		}*/
+		
+		/*@GetMapping("/update/{id}")
+		public String mostrarActualizacion(@PathVariable("id") int idusuario, Model model) {
+			Optional<Usuarios> usuario = repo.findById(idusuario);
+			if (usuario.isPresent()) {
+				model.addAttribute("usuario", usuario);
+				return "crudusuarios/actualizar";
+			}else {
+				return "redirect:/users/";
+			}
+			//return "crudusuarios/actualizar";
+		}*/
+		
+		/*
+		 * Metodo que devuelve desde la base de datos todo los registros y los muestra en la tabla de la vista.
+		 
+		@GetMapping("/")
+		public String Listado(Model model) {
+			List<Usuarios> listusers = new LinkedList<Usuarios>();		
+				Iterable<Usuarios> usuarios = repo.findAll();
+					for (Usuarios usu : usuarios) {
+						listusers.add(usu);
+					}
+					model.addAttribute("usuario", listusers);
+					System.out.println(listusers);
+			
+			return "/crudusuarios/lista";
+		}*/
+		
+		/*
+		 * Metodo Para eliminar todos los registro de la bd (DELETEALL)
+		 */
+		/*@GetMapping("/deleteall")
+		public String Deleteall() {
+			repo.deleteAll();
+			System.out.println("Se han eliminado todos los registros.");
+			
+			return "crudusuarios/lista";
+		}*/
+		
+		/*
+		 * Metodo para eliminar un registro de la bd (DELETE) por el id, en la tabla
+		 */
+		/*@GetMapping("/delete/{id}")
+		public String Eliminar(@PathVariable("id") int idusuario, Model model) {
+			model.addAttribute("idusuario", idusuario);
+			repo.deleteById(idusuario);
+			
+			return "crudusuarios/eliminar";		
+		}*/
+		
+		/*
+		 * Metodo para ver detalles de registros en la tabla (READ)
+		 */
+		/*@GetMapping("/detalle/{id}")
+		public String buscarid(@PathVariable("id") int idusuario, Model model) {
+			model.addAttribute("idusuario", idusuario);
+			//Usuarios ustmp = new Usuarios();
+			Optional<Usuarios> opt = repo.findById(idusuario);
+			if (opt.isPresent()) {
+				model.addAttribute("usuario", opt.get());
+				System.out.println("USUARIO: "+opt.get());
+				return "crudusuarios/detalle";
+			}else {
+				System.out.println("Coloque un id para buscar");
+			}
+			return "";
+		}*/
+		
+	/*
+	 * METODOS TIPO POST
+	 */
+		
+		/*
+		 * Método de actualización de registro en la bd (UPDATE)
+		 
+		@PostMapping("/actualizado")
+		public String Actualizar(@RequestParam("nombre") String nombretmp, @RequestParam("apellido") String apellidotmp, @RequestParam("nusuario") String nusuariotmp, @RequestParam("passwork") String passworktmp, @RequestParam("genero") String generotmp, @PathVariable("id") int idusuario, Model model) {
+			model.addAttribute("idusuario", idusuario);
+			Optional<Usuarios> option = repo.findById(idusuario);
+			if (option.isPresent()) {
+				Usuarios usertemp = option.get();
+				usertemp.setNombre(nombretmp);
+				usertemp.setApellido(apellidotmp);
+				usertemp.setNusuario(nusuariotmp);
+				usertemp.setPasswork(passworktmp);
+				usertemp.setGenero(generotmp);
+					repo.save(usertemp);
+				System.out.println(option.get());
+				return "redirect:/users/";
+			} else {
+				System.out.println("Usuario no encontrado.");
+				return "crudusuarios/actualizar";
+			}		
+		}*/
+		
+		/* *
+		 * Método de registro en la bd (CREATE).
+		 * 
+		@PostMapping("/registrado")
+		public String Registro(@RequestParam("nombre") String nombretmp, @RequestParam("apellido") String apellidotmp, @RequestParam("nusuario") String nusuariotmp, @RequestParam("passwork") String passworktmp, @RequestParam("genero") String generotmp) {
+			Usuarios usercreate = new Usuarios();
+			usercreate.setNombre(nombretmp);
+			usercreate.setApellido(apellidotmp);
+			usercreate.setNusuario(nusuariotmp);
+			usercreate.setPasswork(passworktmp);
+			usercreate.setGenero(generotmp);
+				repo.save(usercreate);
+			System.out.println(usercreate);
+				
+			return "redirect:/users/";		
+		}*/
+
+
+}
